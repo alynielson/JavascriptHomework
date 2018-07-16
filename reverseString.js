16853b762f5615f0a7c8e76d4461f0569b410ced
alynielson <nielsonaly@gmail.com>
2018-07-16 08:56:41 -0500

    Fix reverseString to use functions

--- reverseString.js
+++ reverseString.js
@@ -1,12 +1,24 @@
 "use strict";
 
-let originalString = prompt("Type anything here:");
-let newString = "";
-let stringToAdd = "";
-
-for (let i=originalString.length; i >= 1; i--) {
-    stringToAdd = originalString[i-1];
-    newString = newString + stringToAdd;
+function getTextFromUser() {
+    let originalString = prompt("Type anything here:");
+    return originalString;  
 }
 
-console.log(newString);
+function reverseString(originalString) {  
+    let newString = "";
+    let stringToAdd = "";
+    for (let i=originalString.length; i >= 1; i--) {
+        stringToAdd = originalString[i-1];
+        newString = newString + stringToAdd;
+    }
+    return newString;
+}
+
+function displayReversedString(originalString) {
+    originalString =  getTextFromUser();
+    let reversedString = reverseString(originalString);
+    console.log(reversedString);
+}
+
+displayReversedString();
